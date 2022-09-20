db7134bf353f484f5d8b7a45aee514784c9fcd62
PatriciaDivi <patriciavianad@gmail.com>
2022-09-19 23:42:33 -0300

    feat: Crie uma rota para o endpoint /cars onde seja possível cadastrar um novo carro

--- src/helpers/globalError.ts
+++ src/helpers/globalError.ts
@@ -2,13 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { CustomerError } from './customerError';
 
 class GlobalError {
-  // eslint-disable-next-line class-methods-use-this
-  public handle(error: CustomerError, _req: Request, res: Response, _next: NextFunction) {
+  public handle = (error: CustomerError, _req: Request, res: Response, _next: NextFunction) => {
     if (error instanceof CustomerError) {
       return res.status(error.status).json({ message: error.message });
     }
     return res.status(500).json({ message: `>>>> ${error}` });
-  }
+  };
 }
 
 export default new GlobalError();
